60843c8151be912f0e45a23eaf124bcea98f54ba
vilarj <lamaravillasan@outlook.com>
2023-07-01 08:25:51 -0400

    Added routes

---
+++ src/app/baseball/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Baseball() {
+  return (
+    <div>
+      <h1>Baseball</h1>
+    </div>
+  );
+}
---
+++ src/app/youtube/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Youtube() {
+  return (
+    <div>
+        <h1>Youtube Channel</h1>
+    </div>
+  )
+}
