d799b4b95cb6aadf2bd1282358d49613c81a2cc3
vilarj <lamaravillasan@outlook.com>
2023-07-02 10:59:56 -0400

    mobile view

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -4,19 +4,21 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <div className="bg-zinc-900">
-      <nav className="flex justify-between items-center w-[92%]  mx-auto text-white">
-        <div className="nav-links duration-500 md:static absolute bg-zinc-900 md:min-h-fit min-h-[60vh] left-0 top-[-100%] md:w-auto  w-full flex items-center px-5">
+      <nav className="text-white">
+        <div>
+          <span className="text-3xl cursor-pointer mx-2 md:hidden block"></span>
+
           <ul className="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8">
-            <li className="text-blue-200 hover:text-teal-200">
+            <li className="mx-4 my-6 md:my-0 text-blue-200 hover:text-teal-200">
               <Link href="/">Home</Link>
             </li>
-            <li className="text-blue-200 hover:text-teal-300">
+            <li className="mx-4 my-6 md:my-0 text-blue-200 hover:text-teal-300">
               <Link href="/projects">Projects</Link>
             </li>
-            <li className="text-blue-200 hover:text-teal-400">
+            <li className="mx-4 my-6 md:my-0 text-blue-200 hover:text-teal-400">
               <Link href="/contact">Contact</Link>
             </li>
-            <li className="text-blue-200 hover:text-teal-500">
+            <li className="mx-4 my-6 md:my-0 text-blue-200 hover:text-teal-500">
               <Link href="/youtube">Youtube</Link>
             </li>
           </ul>
