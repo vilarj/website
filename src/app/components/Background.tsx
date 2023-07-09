dd2fd9cf9be067305501e23ce34d12a3e8bdc213
vilarj <lamaravillasan@outlook.com>
2023-07-09 15:40:33 -0400

    Renaming the background component

--- src/app/components/Background.tsx
+++ src/app/components/Background.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Body() {
+export default function Background() {
   return (
     <body className="bg-gradient-to-t from-[#68faff] to-[#74a3f5] h-screen" />
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
-import Body from "./components/Background";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import Background from "./components/Background";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Home() {
   return (
     <div>
       <ToastContainer />
       <Navbar />
-      <Body />
+      <Background />
       <Footer />
     </div>
   );
